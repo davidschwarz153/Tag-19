0ebbd12c812c44fab13816f24a359fa0226c1b43
davidschwarz153 <schwarz.david100@gmail.com>
2024-12-11 16:14:13 +0100

    m

--- If-else/2_3/main.js
+++ If-else/2_3/main.js
@@ -5,10 +5,11 @@ function oi(num){
     if (ts > 27 ) {
         console.log((num - ts) * 2) ;
     } else {
-        console.log(num - ts);
+        console.log(Math.abs(num - ts));
     }
 }
 
 oi(35)
 oi(74)
 oi(123)
+oi(2)
