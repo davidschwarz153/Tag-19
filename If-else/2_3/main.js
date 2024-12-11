a6c9fd1df50864c3304dadeeb4c5d5a0e34b6360
davidschwarz153 <schwarz.david100@gmail.com>
2024-12-11 16:12:35 +0100

    m

---
+++ If-else/2_3/main.js
@@ -0,0 +1,14 @@
+function oi(num){
+    const ts = 27
+    
+
+    if (ts > 27 ) {
+        console.log((num - ts) * 2) ;
+    } else {
+        console.log(num - ts);
+    }
+}
+
+oi(35)
+oi(74)
+oi(123)
